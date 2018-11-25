92f3d79969df89e743a8603866c843407a88b869
soochowcjf <840003625@qq.com>
2018-11-25 19:45:13 +0800

    netty中设计模式的应用

---
+++ src/main/java/com/cjf/netty/chapter11/IterableTest.java
@@ -0,0 +1,39 @@
+package com.cjf.netty.chapter11;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufAllocator;
+import io.netty.buffer.CompositeByteBuf;
+import io.netty.buffer.Unpooled;
+
+/**
+ * created by cjf 21:17 2018/11/24
+ * <p>
+ * 迭代器模式
+ */
+public class IterableTest {
+
+
+    public static void main(String[] args) {
+        ByteBuf header = Unpooled.wrappedBuffer(new byte[]{1, 2, 3});
+        ByteBuf body = Unpooled.wrappedBuffer(new byte[]{4, 5, 6});
+
+        ByteBuf merge = merge(header, body);
+        merge.forEachByte(value -> {
+            System.out.println(value);
+            return true;
+        });
+
+
+    }
+
+    /**
+     * 可以实现内存的零拷贝
+     */
+    public static ByteBuf merge(ByteBuf header, ByteBuf body) {
+        CompositeByteBuf byteBuf = ByteBufAllocator.DEFAULT.compositeBuffer(2);
+        byteBuf.addComponent(true, header);
+        byteBuf.addComponent(true, body);
+
+        return byteBuf;
+    }
+}
---
+++ src/main/java/com/cjf/netty/chapter11/Strategy.java
@@ -0,0 +1,50 @@
+package com.cjf.netty.chapter11;
+
+import io.netty.util.concurrent.DefaultEventExecutorChooserFactory;
+import io.netty.util.concurrent.EventExecutor;
+
+/**
+ * created by cjf 15:21 2018/11/24
+ * <p>
+ * 策略模式
+ *
+ * @see DefaultEventExecutorChooserFactory#newChooser(EventExecutor[])
+ */
+public class Strategy {
+
+    private Cache cacheMemory = new CacheMemoryImpl();
+    private Cache cacheRedis = new CacheRedisImpl();
+
+    public interface Cache {
+        boolean add(String key, String value);
+    }
+
+    public class CacheMemoryImpl implements Cache {
+
+        @Override
+        public boolean add(String key, String value) {
+            //保存到内存中
+            return true;
+        }
+    }
+
+    public class CacheRedisImpl implements Cache {
+
+        @Override
+        public boolean add(String key, String value) {
+            //保存到redis中
+            return true;
+        }
+    }
+
+    /**
+     * 选择哪一种缓存策略
+     */
+    public Cache getCache(String key) {
+        if (key.length() < 10) {
+            return cacheRedis;
+        }
+        return cacheMemory;
+    }
+
+}
