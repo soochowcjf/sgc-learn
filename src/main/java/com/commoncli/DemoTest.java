176d9c40bd02e98e627f306b18f6c70982bbad6e
chenjf <OuosxAygJw>
2020-03-26 12:59:20 +0800

    cli

---
+++ src/main/java/com/commoncli/DemoTest.java
@@ -0,0 +1,12 @@
+package com.commoncli;
+
+/**
+ * @author cjf on 2020/3/26 12:47
+ */
+public class DemoTest {
+
+    public static void main(String[] args) throws Exception {
+        String[] args1 = {"-t 1000"};
+        CLI.main(args1);
+    }
+}
