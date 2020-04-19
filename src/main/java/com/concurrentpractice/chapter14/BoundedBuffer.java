7e2b42c0d6b8d14bc99d40384116bb524f5edbd4
chenjf <OuosxAygJw>
2020-04-19 19:51:17 +0800

    并发编程chapter14

---
+++ src/main/java/com/concurrentpractice/chapter14/BoundedBuffer.java
@@ -0,0 +1,56 @@
+package com.concurrentpractice.chapter14;
+
+import javax.annotation.concurrent.ThreadSafe;
+
+/**
+ * BoundedBuffer
+ * <p/>
+ * Bounded buffer using condition queues
+ *
+ * @author Brian Goetz and Tim Peierls
+ */
+@ThreadSafe
+public class BoundedBuffer<V> extends BaseBoundedBuffer<V> {
+    // CONDITION PREDICATE: not-full (!isFull())
+    // CONDITION PREDICATE: not-empty (!isEmpty())
+    public BoundedBuffer() {
+        this(100);
+    }
+
+    public BoundedBuffer(int size) {
+        super(size);
+    }
+
+    // BLOCKS-UNTIL: not-full
+    public synchronized void put(V v) throws InterruptedException {
+        while (isFull()) {
+            //会释放锁
+            wait();
+        }
+        doPut(v);
+        notifyAll();
+    }
+
+    // BLOCKS-UNTIL: not-empty
+    public synchronized V take() throws InterruptedException {
+        while (isEmpty()) {
+            wait();
+        }
+        V v = doTake();
+        notifyAll();
+        return v;
+    }
+
+    // BLOCKS-UNTIL: not-full
+    // Alternate form of put() using conditional notification
+    public synchronized void alternatePut(V v) throws InterruptedException {
+        while (isFull()) {
+            wait();
+        }
+        boolean wasEmpty = isEmpty();
+        doPut(v);
+        if (wasEmpty) {
+            notifyAll();
+        }
+    }
+}
---
+++ src/main/java/com/concurrentpractice/chapter14/Demo.java
@@ -0,0 +1,11 @@
+package com.concurrentpractice.chapter14;
+
+/**
+ * @author cjf on 2020/4/19 16:30
+ */
+public class Demo {
+    public static void main(String[] args) {
+
+//        new BaseBoundedBuffer<Integer>(10);
+    }
+}
